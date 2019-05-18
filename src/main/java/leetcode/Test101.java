4743b7d68491530c97b51b0f8bb4380343594b88
Coin Lee <coins@coindemacbook-pro.local>
2019-05-18 16:59:05 +0800

    third week

---
+++ src/main/java/leetcode/Test101.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * DESCRIPTION:判断二叉树是不是对称的
+ *
+ * @author Coins
+ * @create 2019-05-18 16:56
+ */
+public class Test101 {
+	public boolean isSymmetric(TreeNode root) {
+		if(root == null) return true;
+		//把问题变成判断两棵树是否是对称的
+		return isSym(root.left, root.right);
+	}
+	//判断的是根节点为r1和r2的两棵树是否是对称的
+	public boolean isSym(TreeNode r1, TreeNode r2){
+		if(r1 == null && r2 == null) return true;
+		if(r1 == null || r2 == null) return false;
+		//这两棵树是对称需要满足的条件：
+		//1.俩根节点相等。 2.树1的左子树和树2的右子树，树2的左子树和树1的右子树都得是对称的
+		return r1.val == r2.val && isSym(r1.left, r2.right)
+				&& isSym(r1.right, r2.left);
+	}
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+}
