b53addc6597698286bd3cf6f21396d6b9a67d4ba
greatsunshine <42766850+greatsunshine@users.noreply.github.com>
2019-06-23 22:19:52 +0800

    Create Test145.java

---
+++ src/main/java/leetcode/Test145.java
@@ -0,0 +1,16 @@
+public class Test145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<Integer>();
+    if(root == null)
+        return res;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.push(root);
+    while(!stack.isEmpty()){
+        TreeNode node = stack.pop();
+        if(node.left != null) stack.push(node.left);//和传统先序遍历不一样，先将左结点入栈
+        if(node.right != null) stack.push(node.right);//后将右结点入栈
+        res.add(0,node.val);                        //逆序添加结点值
+    }     
+    return res;
+   }
+}
